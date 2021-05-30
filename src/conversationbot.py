72061fd5169540397ee01229f8e57758b9434687
Giuseppe Latrofa <latrofagpp@gmail.com>
2021-05-30 18:52:25 +0200

    modified template code

--- src/conversationbot.py
+++ src/conversationbot.py
@@ -36,90 +36,51 @@ logging.basicConfig(
 
 logger = logging.getLogger(__name__)
 
-GENDER, PHOTO, LOCATION, BIO = range(4)
+PIZZA, RECOMENDATION, EXPLANATION, RATING = range(4)
 
 
 def start(update: Update, _: CallbackContext) -> int:
     """Starts the conversation and asks the user about their gender."""
-    reply_keyboard = [['Boy', 'Girl', 'Other']]
+    reply_keyboard = [['Pizza 1', 'Pizza 2']]
 
     update.message.reply_text(
-        'Hi! My name is Professor Bot. I will hold a conversation with you. '
+        'Hi! I\'m a test bot.\n'
         'Send /cancel to stop talking to me.\n\n'
-        'Are you a boy or a girl?',
+        'Please, choose a pizza from this list <link_here>:',
         reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True),
     )
 
-    return GENDER
+    return RECOMENDATION
 
 
-def gender(update: Update, _: CallbackContext) -> int:
-    """Stores the selected gender and asks for a photo."""
+def pizza_suggestion(update: Update, _: CallbackContext):
     user = update.message.from_user
-    logger.info("Gender of %s: %s", user.first_name, update.message.text)
+    logger.info("User name: %s, user choise: %s", user.first_name, update.message.text)
     update.message.reply_text(
-        'I see! Please send me a photo of yourself, '
-        'so I know what you look like, or send /skip if you don\'t want to.',
+        'This is my recomendation: bla bla\n\n'
+        'Please, reate this recomendation:',
+        reply_markup=ReplyKeyboardMarkup([['1', '2', '3', '4', '5']], one_time_keyboard=True),
+    )
+    
+    return EXPLANATION
+
+
+def user_explanation(update: Update, _: CallbackContext):
+    logger.info("User rating: %s", update.message.text)
+    update.message.reply_text(
+        'Got it, thanks for reviewing me.\n\n'
+        'Please, explain your rating:',
+    )
+    
+    return RATING
+
+
+def end_conversation(update: Update, _: CallbackContext):
+    logger.info("User rating explanation: %s", update.message.text)
+    update.message.reply_text(
+        'Thank you!',
         reply_markup=ReplyKeyboardRemove(),
     )
-
-    return PHOTO
-
-
-def photo(update: Update, _: CallbackContext) -> int:
-    """Stores the photo and asks for a location."""
-    user = update.message.from_user
-    photo_file = update.message.photo[-1].get_file()
-    photo_file.download('user_photo.jpg')
-    logger.info("Photo of %s: %s", user.first_name, 'user_photo.jpg')
-    update.message.reply_text(
-        'Gorgeous! Now, send me your location please, or send /skip if you don\'t want to.'
-    )
-
-    return LOCATION
-
-
-def skip_photo(update: Update, _: CallbackContext) -> int:
-    """Skips the photo and asks for a location."""
-    user = update.message.from_user
-    logger.info("User %s did not send a photo.", user.first_name)
-    update.message.reply_text(
-        'I bet you look great! Now, send me your location please, or send /skip.'
-    )
-
-    return LOCATION
-
-
-def location(update: Update, _: CallbackContext) -> int:
-    """Stores the location and asks for some info about the user."""
-    user = update.message.from_user
-    user_location = update.message.location
-    logger.info(
-        "Location of %s: %f / %f", user.first_name, user_location.latitude, user_location.longitude
-    )
-    update.message.reply_text(
-        'Maybe I can visit you sometime! At last, tell me something about yourself.'
-    )
-
-    return BIO
-
-
-def skip_location(update: Update, _: CallbackContext) -> int:
-    """Skips the location and asks for info about the user."""
-    user = update.message.from_user
-    logger.info("User %s did not send a location.", user.first_name)
-    update.message.reply_text(
-        'You seem a bit paranoid! At last, tell me something about yourself.'
-    )
-
-    return BIO
-
-
-def bio(update: Update, _: CallbackContext) -> int:
-    """Stores the info about the user and ends the conversation."""
-    user = update.message.from_user
-    logger.info("Bio of %s: %s", user.first_name, update.message.text)
-    update.message.reply_text('Thank you! I hope we can talk again some day.')
 
     return ConversationHandler.END
 
@@ -129,7 +90,9 @@ def cancel(update: Update, _: CallbackContext) -> int:
     user = update.message.from_user
     logger.info("User %s canceled the conversation.", user.first_name)
     update.message.reply_text(
-        'Bye! I hope we can talk again some day.', reply_markup=ReplyKeyboardRemove()
+        'Conversation canceled.\n'
+        'Please type /start to restart the conversation.',
+        reply_markup=ReplyKeyboardRemove()
     )
 
     return ConversationHandler.END
@@ -147,13 +110,9 @@ def main() -> None:
     conv_handler = ConversationHandler(
         entry_points=[CommandHandler('start', start)],
         states={
-            GENDER: [MessageHandler(Filters.regex('^(Boy|Girl|Other)$'), gender)],
-            PHOTO: [MessageHandler(Filters.photo, photo), CommandHandler('skip', skip_photo)],
-            LOCATION: [
-                MessageHandler(Filters.location, location),
-                CommandHandler('skip', skip_location),
-            ],
-            BIO: [MessageHandler(Filters.text & ~Filters.command, bio)],
+            RECOMENDATION: [MessageHandler(Filters.regex('^(Pizza 1|Pizza 2)$'), pizza_suggestion)],
+            EXPLANATION: [MessageHandler(Filters.regex('^(1|2|3|4|5)$'), user_explanation)],
+            RATING: [MessageHandler(Filters.text & ~Filters.command, end_conversation)],
         },
         fallbacks=[CommandHandler('cancel', cancel)],
     )
